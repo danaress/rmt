8835e74a9b80ac6a58b69dd8500da03375fb4584
Student <ressdana@gmail.com>
2016-03-08 17:17:24 -0700

    almost done

--- worker.js
+++ worker.js
@@ -18,7 +18,7 @@ console.log("worker working!!!!!");
 			{
 			    //get the mins of the current time
 			    var mins = new Date().getMinutes();
-			    if(mins == 14){
+			    if(mins == 19){
 				var datex = new Date().getHours() - 2;
 				console.log(datex)
 				console.log("got this far")
