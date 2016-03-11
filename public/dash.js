b5dca191dd99268a08fe172c36e8170d58e45041
Student <ressdana@gmail.com>
2016-03-11 16:47:38 -0700

    dd

--- public/dash.js
+++ public/dash.js
@@ -47,7 +47,7 @@ angular.module('myApp')
             console.log(d.getDay())
             $scope.FinalH1WeekY = Math.round(100*($scope.H1WeekY/7))
             $scope.FinalH1WeekN = Math.round(100*($scope.H1WeekN/7))
-            $scope.FinalH1Week0 = Math.round(10*(7-($scope.FinalH1WeekN + $scope.FinalH1WeekY)))
+            $scope.FinalH1Week0 = Math.round(10*(7-($scope.H1WeekN + $scope.H1WeekY)))
             $scope.loadChartH1Week();
         }
 
