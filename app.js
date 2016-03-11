877d744f6bccf4e49a137f2cb9c852f242b39edb
Student <ressdana@gmail.com>
2016-03-11 11:13:52 -0700

    dates

--- app.js
+++ app.js
@@ -126,7 +126,7 @@ app.get('/', function(req, res){
 
 app.post('/test', function (req, res) {
 	var x = req.body.Body.split(' ')
-    console.log(Date.parse('req.body.Date'))
+    console.log(Date.parse(req.body.Date))
     var y = Date.parse('req.body.Date')
     console.log("This is the date: " + y)
 		var num = req.body.From
