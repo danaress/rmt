30482b0d3f17dc4b61947b5b5a837907f71319d2
Student <ressdana@gmail.com>
2016-03-11 15:20:59 -0700

    testing 1week graph

--- public/dash.js
+++ public/dash.js
@@ -11,8 +11,8 @@ angular.module('myApp')
         $scope.FinalH1WeekY = 0
         $scope.FinalH1WeekN = 0
 
-            //Function for Chart 1
 
+        //Habit 1 LAST WEEK
         $scope.array1Week = function(){
             console.log($scope.userInfo[0]);
             console.log("length = " + $scope.userInfo[0].array1.length)
@@ -45,12 +45,11 @@ angular.module('myApp')
             console.log(d.getDay())
             $scope.FinalH1WeekY = (100*($scope.H1WeekY/($scope.H1WeekN+$scope.H1WeekY)))
             $scope.FinalH1WeekN = (100*($scope.H1WeekN/($scope.H1WeekN+$scope.H1WeekY)))
-            console.log("This is the final Y for 1 week " + $scope.FinalH1WeekY)
-            console.log("This is the final N for 1 week " + $scope.FinalH1WeekN)
+            $scope.FinalH1Week0 = (10*(7-($scope.FinalH1WeekN + $scope.FinalH1WeekY))
         }
 
 
-
+        // Habit 1 ALL TIME
         $scope.array1Metrics = function(){
             console.log($scope.userInfo[0]);
             console.log("length = " + $scope.userInfo[0].array1.length)
@@ -78,7 +77,7 @@ angular.module('myApp')
         }
         ///////End
 
-            // Function for Chart 2
+        //Habit 2 ALL TIME
         $scope.array2Metrics = function(){
             console.log("length = " + $scope.userInfo[0].array2.length)
             $scope.H2Y = 0
@@ -98,7 +97,7 @@ angular.module('myApp')
         }
 
 
-                //Function for Chart 3
+        //Habit 3 ALL TIME
         $scope.array3Metrics = function(){
             console.log("length = " + $scope.userInfo[0].array3.length)
             $scope.H3Y = 0
@@ -136,7 +135,9 @@ angular.module('myApp')
         }
         $scope.getMetrics()
 
-// /////////////////////////////////////// HighCharts Activity Gauge for Array 1 ///////////////////////////////
+
+
+// /////////////////////////////////////// HighCharts Activity Gauge for Array 1  in LAST 7 DAYS ///////////////////////////////
 $scope.loadChart1 = function(){
         $(function () {
 
@@ -152,7 +153,161 @@ $scope.loadChart1 = function(){
         },
 
         title: {
-            text: $scope.userInfo[0].habit2,
+            text: $scope.userInfo[0].habit1 'All Time',
+            style: {
+                fontSize: '24px'
+            }
+        },
+
+        tooltip: {
+            borderWidth: 0,
+            backgroundColor: 'none',
+            shadow: false,
+            style: {
+                fontSize: '16px'
+            },
+            pointFormat: '{series.name}<br><span style="font-size:2em; color: {point.color}; font-weight: bold">{point.y}%</span>',
+            positioner: function (labelWidth, labelHeight) {
+                return {
+                    x: 200 - labelWidth / 2,
+                    y: 180
+                };
+            }
+        },
+
+        pane: {
+            startAngle: 0,
+            endAngle: 360,
+            background: [{ // Track for Move
+                outerRadius: '112%',
+                innerRadius: '88%',
+                backgroundColor:'rgba(46, 204, 113, .1)',
+                borderWidth: 0
+            }, { // Track for Exercise
+                outerRadius: '87%',
+                innerRadius: '63%',
+                backgroundColor:'rgba(231, 76, 60, .1)',
+                borderWidth: 0
+            }, { // Track for Stand
+                outerRadius: '62%',
+                innerRadius: '38%',
+                backgroundColor: 'rgba(149, 165, 166, .1)',
+                borderWidth: 0
+            }]
+        },
+
+        yAxis: {
+            min: 0,
+            max: 100,
+            lineWidth: 0,
+            tickPositions: []
+        },
+
+        plotOptions: {
+            solidgauge: {
+                borderWidth: '34px',
+                dataLabels: {
+                    enabled: false
+                },
+                linecap: 'round',
+                stickyTracking: false
+            }
+        },
+
+        series: [{
+            name: 'Goal Met',
+            borderColor: 'rgb(46, 204, 113)',
+            data: [{
+                color: 'rgb(46, 204, 113)',
+                radius: '100%',
+                innerRadius: '100%',
+                y: $scope.FinalH1WeekY
+            }]
+        }, {
+            name: 'Goal Not Met',
+            borderColor: 'rgb(231, 76, 60)',
+            data: [{
+                color: 'rgb(231, 76, 60)',
+                radius: '75%',
+                innerRadius: '75%',
+                y: $scope.FinalH1WeekN
+            }]
+        }, {
+            name: 'Not recorded',
+            borderColor: 'rgb(149, 165, 166)',
+            data: [{
+                color: 'rgb(149, 165, 166)',
+                radius: '50%',
+                innerRadius: '50%',
+                y: FinalH1Week0
+            }]
+        }]
+    },
+
+    /**
+     * In the chart load callback, add icons on top of the circular shapes
+     */
+    function callback() {
+
+        // Move icon
+        this.renderer.path(['M', -8, 0, 'L', 8, 0, 'M', 0, -8, 'L', 8, 0, 0, 8])
+            .attr({
+                'stroke': '#303030',
+                'stroke-linecap': 'round',
+                'stroke-linejoin': 'round',
+                'stroke-width': 2,
+                'zIndex': 10
+            })
+            .translate(190, 26)
+            .add(this.series[2].group);
+
+        // Exercise icon
+        this.renderer.path(['M', -8, 0, 'L', 8, 0, 'M', 0, -8, 'L', 8, 0, 0, 8, 'M', 8, -8, 'L', 16, 0, 8, 8])
+            .attr({
+                'stroke': '#303030',
+                'stroke-linecap': 'round',
+                'stroke-linejoin': 'round',
+                'stroke-width': 2,
+                'zIndex': 10
+            })
+            .translate(190, 61)
+            .add(this.series[2].group);
+
+        // Stand icon
+        this.renderer.path(['M', 0, 8, 'L', 0, -8, 'M', -8, 0, 'L', 0, -8, 8, 0])
+            .attr({
+                'stroke': '#303030',
+                'stroke-linecap': 'round',
+                'stroke-linejoin': 'round',
+                'stroke-width': 2,
+                'zIndex': 10
+            })
+            .translate(190, 96)
+            .add(this.series[2].group);
+    });
+});
+}
+
+
+
+
+// /////////////////////////////////////// HighCharts Activity Gauge for Array 1 of ALL TIME ///////////////////////////////
+$scope.loadChart1 = function(){
+        $(function () {
+
+    Highcharts.chart('container', {
+
+        chart: {
+            type: 'solidgauge',
+            marginTop: 50
+        },
+
+        credits: {
+            enabled: false
+        },
+
+        title: {
+            text: $scope.userInfo[0].habit1,
             style: {
                 fontSize: '24px'
             }
@@ -304,7 +459,7 @@ $scope.loadChart2 = function(){
         },
 
         title: {
-            text: $scope.userInfo[0].habit1,
+            text: $scope.userInfo[0].habit2,
             style: {
                 fontSize: '24px'
             }
