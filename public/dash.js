981efffa88f80f6a1635c180829a149eeb5d0078
Student <ressdana@gmail.com>
2016-03-11 17:44:19 -0700

    testing

--- public/dash.js
+++ public/dash.js
@@ -4,6 +4,7 @@ angular.module('myApp')
 
         $scope.mili = 86399999
         $scope.week = 604800000
+        $scope.month = 2629746000
         $scope.datenow = Date.now()
 
         $scope.FinalH1N = 0
@@ -21,6 +22,99 @@ angular.module('myApp')
         $scope.FinalH3WeekY = 0
         $scope.FinalH3WeekN = 0
         $scope.FinalH3Week0 = 0
+        $scope.FinalH1MonthY = 0
+        $scope.FinalH1MonthN = 0
+        $scope.FinalH1Month0 = 0
+        $scope.FinalH2MonthY = 0
+        $scope.FinalH2MonthN = 0
+        $scope.FinalH2Month0 = 0
+        $scope.FinalH3MonthY = 0
+        $scope.FinalH3MonthN = 0
+        $scope.FinalH3Month0 = 0
+
+
+
+
+        //Habit 1 LAST MONTH
+        $scope.array1Month= function(){
+            console.log($scope.userInfo[0]);
+            console.log("length = " + $scope.userInfo[0].array1.length)
+            $scope.month1Array = []
+            $scope.H1MonthY = 0
+            $scope.H1MonthN = 0
+            for (var i=0; i<$scope.userInfo[0].array1.length; i++){
+                if (($scope.datenow - $scope.userInfo[0].array1[i][1]) < $scope.month){
+            $scope.month1Array.push($scope.userInfo[0].array1[i][0])
+    }
+            }
+            for (var i=0; i < $scope.month1Array.length; i++){
+                if ($scope.month1Array[i] == "y" || $scope.month1Array[i] == "Y"){
+                    $scope.H1MonthY = ($scope.H1MonthY + 1)
+                    
+                } else if ($scope.month1Array[i] == "n" || $scope.month1Array[i] == "N"){
+                    $scope.H1MonthN = ($scope.H1MonthN + 1)
+                }
+            }
+            $scope.FinalH1MonthY = Math.round(100*($scope.H1MonthY/7))
+            $scope.FinalH1MonthN = Math.round(100*($scope.H1MonthN/7))
+            $scope.FinalH1Month0 = Math.round(10*(7-($scope.H1MonthN + $scope.H1MonthY)))
+            $scope.loadChartH1Month();
+        }
+
+        //Habit 2 LAST MONTH
+        $scope.array2Month= function(){
+            console.log($scope.userInfo[0]);
+            console.log("length = " + $scope.userInfo[0].array2.length)
+            $scope.month2Array = []
+            $scope.H2MonthY = 0
+            $scope.H2MonthN = 0
+            for (var i=0; i<$scope.userInfo[0].array2.length; i++){
+                if (($scope.datenow - $scope.userInfo[0].array2[i][1]) < $scope.month){
+            $scope.month2Array.push($scope.userInfo[0].array2[i][0])
+    }
+            }
+            for (var i=0; i < $scope.month2Array.length; i++){
+                if ($scope.month2Array[i] == "y" || $scope.month2Array[i] == "Y"){
+                    $scope.H2MonthY = ($scope.H2MonthY + 1)
+                    
+                } else if ($scope.month2Array[i] == "n" || $scope.month1Array[i] == "N"){
+                    $scope.H2MonthN = ($scope.H2MonthN + 1)
+                }
+            }
+            $scope.FinalH2MonthY = Math.round(100*($scope.H2MonthY/7))
+            $scope.FinalH2MonthN = Math.round(100*($scope.H2MonthN/7))
+            $scope.FinalH2Month0 = Math.round(10*(7-($scope.H2MonthN + $scope.H2MonthY)))
+            $scope.loadChartH2Month();
+        }
+
+        //Habit 3 LAST MONTH
+        $scope.array3Month= function(){
+            console.log($scope.userInfo[0]);
+            console.log("length = " + $scope.userInfo[0].array3.length)
+            $scope.month3Array = []
+            $scope.H3MonthY = 0
+            $scope.H3MonthN = 0
+            for (var i=0; i<$scope.userInfo[0].array3.length; i++){
+                if (($scope.datenow - $scope.userInfo[0].array3[i][1]) < $scope.month){
+            $scope.month3Array.push($scope.userInfo[0].array3[i][0])
+    }
+            }
+            for (var i=0; i < $scope.month3Array.length; i++){
+                if ($scope.month3Array[i] == "y" || $scope.month3Array[i] == "Y"){
+                    $scope.H3MonthY = ($scope.H3MonthY + 1)
+                    
+                } else if ($scope.month2Array[i] == "n" || $scope.month1Array[i] == "N"){
+                    $scope.H3MonthN = ($scope.H3MonthN + 1)
+                }
+            }
+            $scope.FinalH3MonthY = Math.round(100*($scope.H3MonthY/7))
+            $scope.FinalH3MonthN = Math.round(100*($scope.H3MonthN/7))
+            $scope.FinalH3Month0 = Math.round(10*(7-($scope.H3MonthN + $scope.H3MonthY)))
+            $scope.loadChartH3Month();
+        }
+
+
+
 
 
         //Habit 1 LAST WEEK
@@ -183,12 +277,470 @@ angular.module('myApp')
                 $scope.array1Week();
                 $scope.array2Week();
                 $scope.array3Week();
+                $scope.array1Month();
+                $scope.array2Month();
+                $scope.array3Month();
             })
         }
         $scope.getMetrics()
 
 
+// /////////////////////////////////////// HighCharts Activity Gauge for Array 1  in LAST 30 DAYS ///////////////////////////////
+$scope.loadChartH1Month = function(){
+        $(function () {
 
+    Highcharts.chart('H1Month', {
+
+        chart: {
+            type: 'solidgauge',
+            marginTop: 50
+        },
+
+        credits: {
+            enabled: false
+        },
+
+        title: {
+            text: $scope.userInfo[0].habit1,
+            style: {
+                fontSize: '24px'
+            }
+        },
+
+        tooltip: {
+            borderWidth: 0,
+            backgroundColor: 'none',
+            shadow: false,
+            style: {
+                fontSize: '16px'
+            },
+            pointFormat: '{series.name}<br><span style="font-size:2em; color: {point.color}; font-weight: bold">{point.y}%</span>',
+            positioner: function (labelWidth, labelHeight) {
+                return {
+                    x: 200 - labelWidth / 2,
+                    y: 180
+                };
+            }
+        },
+
+        pane: {
+            startAngle: 0,
+            endAngle: 360,
+            background: [{ // Track for Move
+                outerRadius: '112%',
+                innerRadius: '88%',
+                backgroundColor:'rgba(46, 204, 113, .1)',
+                borderWidth: 0
+            }, { // Track for Exercise
+                outerRadius: '87%',
+                innerRadius: '63%',
+                backgroundColor:'rgba(231, 76, 60, .1)',
+                borderWidth: 0
+            }, { // Track for Stand
+                outerRadius: '62%',
+                innerRadius: '38%',
+                backgroundColor: 'rgba(149, 165, 166, .1)',
+                borderWidth: 0
+            }]
+        },
+
+        yAxis: {
+            min: 0,
+            max: 100,
+            lineWidth: 0,
+            tickPositions: []
+        },
+
+        plotOptions: {
+            solidgauge: {
+                borderWidth: '34px',
+                dataLabels: {
+                    enabled: false
+                },
+                linecap: 'round',
+                stickyTracking: false
+            }
+        },
+
+        series: [{
+            name: 'Goal Met',
+            borderColor: 'rgb(46, 204, 113)',
+            data: [{
+                color: 'rgb(46, 204, 113)',
+                radius: '100%',
+                innerRadius: '100%',
+                y: $scope.FinalH1MonthY
+            }]
+        }, {
+            name: 'Goal Not Met',
+            borderColor: 'rgb(231, 76, 60)',
+            data: [{
+                color: 'rgb(231, 76, 60)',
+                radius: '75%',
+                innerRadius: '75%',
+                y: $scope.FinalH1MonthN
+            }]
+        }, {
+            name: 'Not recorded',
+            borderColor: 'rgb(149, 165, 166)',
+            data: [{
+                color: 'rgb(149, 165, 166)',
+                radius: '50%',
+                innerRadius: '50%',
+                y: $scope.FinalH1Month0
+            }]
+        }]
+    },
+
+    /**
+     * In the chart load callback, add icons on top of the circular shapes
+     */
+    function callback() {
+
+        // Move icon
+        this.renderer.path(['M', -8, 0, 'L', 8, 0, 'M', 0, -8, 'L', 8, 0, 0, 8])
+            .attr({
+                'stroke': '#303030',
+                'stroke-linecap': 'round',
+                'stroke-linejoin': 'round',
+                'stroke-width': 2,
+                'zIndex': 10
+            })
+            .translate(190, 26)
+            .add(this.series[2].group);
+
+        // Exercise icon
+        this.renderer.path(['M', -8, 0, 'L', 8, 0, 'M', 0, -8, 'L', 8, 0, 0, 8, 'M', 8, -8, 'L', 16, 0, 8, 8])
+            .attr({
+                'stroke': '#303030',
+                'stroke-linecap': 'round',
+                'stroke-linejoin': 'round',
+                'stroke-width': 2,
+                'zIndex': 10
+            })
+            .translate(190, 61)
+            .add(this.series[2].group);
+
+        // Stand icon
+        this.renderer.path(['M', 0, 8, 'L', 0, -8, 'M', -8, 0, 'L', 0, -8, 8, 0])
+            .attr({
+                'stroke': '#303030',
+                'stroke-linecap': 'round',
+                'stroke-linejoin': 'round',
+                'stroke-width': 2,
+                'zIndex': 10
+            })
+            .translate(190, 96)
+            .add(this.series[2].group);
+    });
+});
+}
+
+
+
+// /////////////////////////////////////// HighCharts Activity Gauge for Array 2  in LAST 30 DAYS ///////////////////////////////
+$scope.loadChartH2Month = function(){
+        $(function () {
+
+    Highcharts.chart('H2Month', {
+
+        chart: {
+            type: 'solidgauge',
+            marginTop: 50
+        },
+
+        credits: {
+            enabled: false
+        },
+
+        title: {
+            text: $scope.userInfo[0].habit2,
+            style: {
+                fontSize: '24px'
+            }
+        },
+
+        tooltip: {
+            borderWidth: 0,
+            backgroundColor: 'none',
+            shadow: false,
+            style: {
+                fontSize: '16px'
+            },
+            pointFormat: '{series.name}<br><span style="font-size:2em; color: {point.color}; font-weight: bold">{point.y}%</span>',
+            positioner: function (labelWidth, labelHeight) {
+                return {
+                    x: 200 - labelWidth / 2,
+                    y: 180
+                };
+            }
+        },
+
+        pane: {
+            startAngle: 0,
+            endAngle: 360,
+            background: [{ // Track for Move
+                outerRadius: '112%',
+                innerRadius: '88%',
+                backgroundColor:'rgba(46, 204, 113, .1)',
+                borderWidth: 0
+            }, { // Track for Exercise
+                outerRadius: '87%',
+                innerRadius: '63%',
+                backgroundColor:'rgba(231, 76, 60, .1)',
+                borderWidth: 0
+            }, { // Track for Stand
+                outerRadius: '62%',
+                innerRadius: '38%',
+                backgroundColor: 'rgba(149, 165, 166, .1)',
+                borderWidth: 0
+            }]
+        },
+
+        yAxis: {
+            min: 0,
+            max: 100,
+            lineWidth: 0,
+            tickPositions: []
+        },
+
+        plotOptions: {
+            solidgauge: {
+                borderWidth: '34px',
+                dataLabels: {
+                    enabled: false
+                },
+                linecap: 'round',
+                stickyTracking: false
+            }
+        },
+
+        series: [{
+            name: 'Goal Met',
+            borderColor: 'rgb(46, 204, 113)',
+            data: [{
+                color: 'rgb(46, 204, 113)',
+                radius: '100%',
+                innerRadius: '100%',
+                y: $scope.FinalH2MonthY
+            }]
+        }, {
+            name: 'Goal Not Met',
+            borderColor: 'rgb(231, 76, 60)',
+            data: [{
+                color: 'rgb(231, 76, 60)',
+                radius: '75%',
+                innerRadius: '75%',
+                y: $scope.FinalH2MonthN
+            }]
+        }, {
+            name: 'Not recorded',
+            borderColor: 'rgb(149, 165, 166)',
+            data: [{
+                color: 'rgb(149, 165, 166)',
+                radius: '50%',
+                innerRadius: '50%',
+                y: $scope.FinalH2Month0
+            }]
+        }]
+    },
+
+    /**
+     * In the chart load callback, add icons on top of the circular shapes
+     */
+    function callback() {
+
+        // Move icon
+        this.renderer.path(['M', -8, 0, 'L', 8, 0, 'M', 0, -8, 'L', 8, 0, 0, 8])
+            .attr({
+                'stroke': '#303030',
+                'stroke-linecap': 'round',
+                'stroke-linejoin': 'round',
+                'stroke-width': 2,
+                'zIndex': 10
+            })
+            .translate(190, 26)
+            .add(this.series[2].group);
+
+        // Exercise icon
+        this.renderer.path(['M', -8, 0, 'L', 8, 0, 'M', 0, -8, 'L', 8, 0, 0, 8, 'M', 8, -8, 'L', 16, 0, 8, 8])
+            .attr({
+                'stroke': '#303030',
+                'stroke-linecap': 'round',
+                'stroke-linejoin': 'round',
+                'stroke-width': 2,
+                'zIndex': 10
+            })
+            .translate(190, 61)
+            .add(this.series[2].group);
+
+        // Stand icon
+        this.renderer.path(['M', 0, 8, 'L', 0, -8, 'M', -8, 0, 'L', 0, -8, 8, 0])
+            .attr({
+                'stroke': '#303030',
+                'stroke-linecap': 'round',
+                'stroke-linejoin': 'round',
+                'stroke-width': 2,
+                'zIndex': 10
+            })
+            .translate(190, 96)
+            .add(this.series[2].group);
+    });
+});
+}
+
+
+
+// /////////////////////////////////////// HighCharts Activity Gauge for Array 3  in LAST 30 DAYS ///////////////////////////////
+$scope.loadChartH3Month = function(){
+        $(function () {
+
+    Highcharts.chart('H3Month', {
+
+        chart: {
+            type: 'solidgauge',
+            marginTop: 50
+        },
+
+        credits: {
+            enabled: false
+        },
+
+        title: {
+            text: $scope.userInfo[0].habit3,
+            style: {
+                fontSize: '24px'
+            }
+        },
+
+        tooltip: {
+            borderWidth: 0,
+            backgroundColor: 'none',
+            shadow: false,
+            style: {
+                fontSize: '16px'
+            },
+            pointFormat: '{series.name}<br><span style="font-size:2em; color: {point.color}; font-weight: bold">{point.y}%</span>',
+            positioner: function (labelWidth, labelHeight) {
+                return {
+                    x: 200 - labelWidth / 2,
+                    y: 180
+                };
+            }
+        },
+
+        pane: {
+            startAngle: 0,
+            endAngle: 360,
+            background: [{ // Track for Move
+                outerRadius: '112%',
+                innerRadius: '88%',
+                backgroundColor:'rgba(46, 204, 113, .1)',
+                borderWidth: 0
+            }, { // Track for Exercise
+                outerRadius: '87%',
+                innerRadius: '63%',
+                backgroundColor:'rgba(231, 76, 60, .1)',
+                borderWidth: 0
+            }, { // Track for Stand
+                outerRadius: '62%',
+                innerRadius: '38%',
+                backgroundColor: 'rgba(149, 165, 166, .1)',
+                borderWidth: 0
+            }]
+        },
+
+        yAxis: {
+            min: 0,
+            max: 100,
+            lineWidth: 0,
+            tickPositions: []
+        },
+
+        plotOptions: {
+            solidgauge: {
+                borderWidth: '34px',
+                dataLabels: {
+                    enabled: false
+                },
+                linecap: 'round',
+                stickyTracking: false
+            }
+        },
+
+        series: [{
+            name: 'Goal Met',
+            borderColor: 'rgb(46, 204, 113)',
+            data: [{
+                color: 'rgb(46, 204, 113)',
+                radius: '100%',
+                innerRadius: '100%',
+                y: $scope.FinalH3MonthY
+            }]
+        }, {
+            name: 'Goal Not Met',
+            borderColor: 'rgb(231, 76, 60)',
+            data: [{
+                color: 'rgb(231, 76, 60)',
+                radius: '75%',
+                innerRadius: '75%',
+                y: $scope.FinalH3MonthN
+            }]
+        }, {
+            name: 'Not recorded',
+            borderColor: 'rgb(149, 165, 166)',
+            data: [{
+                color: 'rgb(149, 165, 166)',
+                radius: '50%',
+                innerRadius: '50%',
+                y: $scope.FinalH3Month0
+            }]
+        }]
+    },
+
+    /**
+     * In the chart load callback, add icons on top of the circular shapes
+     */
+    function callback() {
+
+        // Move icon
+        this.renderer.path(['M', -8, 0, 'L', 8, 0, 'M', 0, -8, 'L', 8, 0, 0, 8])
+            .attr({
+                'stroke': '#303030',
+                'stroke-linecap': 'round',
+                'stroke-linejoin': 'round',
+                'stroke-width': 2,
+                'zIndex': 10
+            })
+            .translate(190, 26)
+            .add(this.series[2].group);
+
+        // Exercise icon
+        this.renderer.path(['M', -8, 0, 'L', 8, 0, 'M', 0, -8, 'L', 8, 0, 0, 8, 'M', 8, -8, 'L', 16, 0, 8, 8])
+            .attr({
+                'stroke': '#303030',
+                'stroke-linecap': 'round',
+                'stroke-linejoin': 'round',
+                'stroke-width': 2,
+                'zIndex': 10
+            })
+            .translate(190, 61)
+            .add(this.series[2].group);
+
+        // Stand icon
+        this.renderer.path(['M', 0, 8, 'L', 0, -8, 'M', -8, 0, 'L', 0, -8, 8, 0])
+            .attr({
+                'stroke': '#303030',
+                'stroke-linecap': 'round',
+                'stroke-linejoin': 'round',
+                'stroke-width': 2,
+                'zIndex': 10
+            })
+            .translate(190, 96)
+            .add(this.series[2].group);
+    });
+});
+}
 
 
 // /////////////////////////////////////// HighCharts Activity Gauge for Array 3  in LAST 7 DAYS ///////////////////////////////
