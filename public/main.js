44352b7e6b94c443535de4724fb8cd8758da740e
Student <ressdana@gmail.com>
2016-03-14 10:08:31 -0600

    et

--- public/main.js
+++ public/main.js
@@ -15,6 +15,36 @@ angular.module('myApp', ['ngRoute'])
 angular.module('myApp')
 	.controller('mainController', ['$scope', '$http', 'userInfo', function($scope, $http, $userInfo){
 
+   $scope.times = {
+            0: '12:00',
+            1: '1:00 AM',
+            2: '2:00 AM',
+            3: '3:00 AM',
+            4: '4:00 AM',
+            5: '5:00 AM',
+            6: '6:00 AM',
+            7: '7:00 AM',
+            8: '8:00 AM',
+            9: '9:00 AM',
+            10: '10:00 AM',
+            11: '11:00 AM',
+            12: '12:00 PM',
+            13: '1:00 PM',
+            14: '2:00 PM',
+            15: '3:00 PM',
+            16: '4:00 PM',
+            17: '5:00 PM',
+            18: '6:00 PM',
+            19: '7:00 PM',
+            20: '8:00 PM',
+            21: '9:00 PM',
+            22: '10:00 PM',
+            23: '11:00 PM'
+        }
+
+
+
+
         $scope.signup = function(){
             $http({ 
                 method : 'POST',
@@ -48,32 +78,7 @@ angular.module('myApp')
         }
 
 
-        $scope.times = {
-            0: '12:00',
-            1: '1:00 AM',
-            2: '2:00 AM',
-            3: '3:00 AM',
-            4: '4:00 AM',
-            5: '5:00 AM',
-            6: '6:00 AM',
-            7: '7:00 AM',
-            8: '8:00 AM',
-            9: '9:00 AM',
-            10: '10:00 AM',
-            11: '11:00 AM',
-            12: '12:00 PM',
-            13: '1:00 PM',
-            14: '2:00 PM',
-            15: '3:00 PM',
-            16: '4:00 PM',
-            17: '5:00 PM',
-            18: '6:00 PM',
-            19: '7:00 PM',
-            20: '8:00 PM',
-            21: '9:00 PM',
-            22: '10:00 PM',
-            23: '11:00 PM',
-        }
+     
 
         $scope.modalInfo = function(req, res){
             $http.post('/metrics')
@@ -87,7 +92,7 @@ angular.module('myApp')
                 $scope.modal2Time = "Please choose a time to receive texts."
                 } else { 
                     console.log($scope.allUserInfo[0].time)
-                    console.log($scope.times.4)
+                    console.log($scope.times.4);
                     $scope.modalTime = $scope.times.$scope.allUserInfo[0].time
                     console.log($scope.modalTime);
                 }
