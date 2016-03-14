b42e58e19fa688aa4807787d213cf7baf28a32f0
Student <ressdana@gmail.com>
2016-03-14 10:55:36 -0600

    d

--- public/main.js
+++ public/main.js
@@ -95,6 +95,8 @@ console.log($scope.times)
                     console.log($scope.times)
                     console.log($scope.times['10'])
                     var x = $scope.allUserInfo[0].time
+                    console.log(x)
+                    console.log(x.toString())
                     console.log($scope.allUserInfo[0].time)
                     console.log($scope.allUserInfo[0].time.toString())
                     console.log(x.toString())
