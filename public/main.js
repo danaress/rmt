c0777d8e2914b729c306d63de1152f64d09c84e9
Student <ressdana@gmail.com>
2016-03-15 11:24:27 -0600

    s

--- public/main.js
+++ public/main.js
@@ -119,35 +119,33 @@ angular.module('myApp')
         }})}
             $scope.welcomeBox()
 
-            $scope.modalInfo = function(req, res){
+            $scope.modalInfo = function(){
             console.log("click worked.")
-            $http.post('/metrics')
-            .then(function(returndata){
-                $scope.allUserInfo = returndata.data
                 //username
-                $scope.modalUsername = $scope.allUserInfo[0].username
-                if ($scope.allUserInfo[0].time == null){
+                $scope.modalUsername = $scope.userCheck[0].username
+                if ($scope.userCheck[0].time == null){
                 //text time
                 $scope.modalTime = "It looks like you haven't selected a time yet - head over to Settings in the top right!"
                 $scope.modal2Time = "Please choose a time to receive texts."
                 } else {
-                    $scope.modalTime = $scope.times[$scope.allUserInfo[0].time]
+                    $scope.modalTime = $scope.times[$scope.userCheck[0].time]
                 }
                 //phone number
-                if ($scope.allUserInfo[0].number == 0){
+                if ($scope.userCheck[0].number == 0){
                     $scope.modal2Number = "You don't have a number saved. Add one below."
                     $scope.modalNumber = "It looks like you haven't added a number yet - head over to Settings in the top right!"
                 } else {
-                    $scope.modal2Number = ("Current number: " + $scope.allUserInfo[0].number)
-                    $scope.modalNumber = $scope.allUserInfo[0].number
+                    $scope.modal2Number = ("Current number: " + $scope.userCheck[0].number)
+                    $scope.modalNumber = $scope.userCheck[0].number
                 }
                 //habits
-                if ($scope.allUserInfo[0].habit1 == ''){
+                if ($scope.userCheck[0].habit1 == ''){
                     $scope.modalHabit = "It looks like you haven't specified any habits yet - head over to Settings in the top right!"
-                } else if ($scope.allUserInfo[0].habit1 == ''){
-                    $scope.modalHabit = ("Habit 1: " + $scope.allUserInfo[0].habit1 + ". Habit 2: " + $scope.allUserInfo[0].habit2 + ". Habit 3: " + $scope.allUserInfo[0].habit3)
+                } else if ($scope.userCheck[0].habit1 == ''){
+                    $scope.modalHabit = ("Habit 1: " + $scope.userCheck[0].habit1 + ". Habit 2: " + $scope.userCheck[0].habit2 + ". Habit 3: " + $scope.allUserInfo[0].habit3)
                 }
-                })
+                }
         }
+
 
         }])
