56cfd98f49c75c2ea43b634266ac72a740f38868
Student <ressdana@gmail.com>
2016-03-15 11:29:23 -0600

    d

--- public/main.js
+++ public/main.js
@@ -135,9 +135,11 @@ angular.module('myApp')
                 }
 
             $scope.welcomeBox = function(req, res){
+                console.log("Function fired")
             $http.post('/welcomeBox')
             .then(function(returndata){
             $scope.userCheck = returndata.data
+            console.log("data back")
             if($scope.userCheck[0].time == null && $scope.userCheck[0].number == 0 && $scope.userCheck[0].habit1 == ''){
             console.log("Load the box")
             $scope.welcomeTime = true;
@@ -145,7 +147,7 @@ angular.module('myApp')
 
         }})}
 
-            $scope.welcomeBox()
+            $scope.welcomeBox();
         
 
 
