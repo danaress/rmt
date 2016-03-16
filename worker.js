8abdc655a0a68826487e1f364c25a657c6f3417e
Student <ressdana@gmail.com>
2016-03-15 18:29:36 -0600

    cleaning up

--- worker.js
+++ worker.js
@@ -4,13 +4,11 @@ var bodyParser = require('body-parser');
 var mongoose = require('mongoose')
 mongoose.connect('mongodb://localhost/users', function(err){
 	if (err) console.log(err)
-	console.log("connected to mongo")
 })
 
 var accountSid = 'AC49f665c07dac0c475d23f634e9df43cb'; 
 var authToken = '2e9a7be1ba9cd9544c2b7739a92c670d';
 var client = require('twilio')(accountSid, authToken);
-console.log("worker working!!!!!");
 
 
 
@@ -23,29 +21,17 @@ console.log("worker working!!!!!");
 				console.log(datex)
 				console.log("got this far")
 				var x = user.find({ time: datex}, function(err, docs){
-					console.log(docs.length)
-				
 			    for (i=0; i < docs.length; i++){
-			    console.log(docs[i].number)
 
 			    client.messages.create({ 
 				to: +docs[i].number, 
 				from: "+13038482330", 
-				body: "'Hey" + docs[i].username + ", did you meet your goals for today? " + docs[i].habit1 + ", " + docs[i].habit2 + ", " + docs[i].habit3 + "? (Y N Y)",   
+				body: "'Hey " + docs[i].username + ", did you meet your goals for today? " + docs[i].habit1 + ", " + docs[i].habit2 + ", " + docs[i].habit3 + "? (Y N Y)",   
 			},  function(err, responseData) { 
-				console.log('done'); 
 			})
 				}})}}
 		tick();
 setInterval(function() { tick(); }, 60000);
-
- // + docs[i].username + ", did you meet your goals for today? " + docs[i].habit1 + ", " + docs[i].habit2 + ", " + docs[i].habit3 + "? (Y N Y)"
-
-// smsTest = function(req, res){
-// var response = req.body.split(' ')
-// console.log(response)
-//  res.sendStatus(200);
-// }
 
 
 
