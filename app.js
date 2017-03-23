e28d4757975a9288c85e7cc76abf9fb437fd0d98
Dana Ress <danaress@MacBook-Air.local>
2017-03-22 18:07:36 -0600

    tttest

--- app.js
+++ app.js
@@ -42,13 +42,11 @@ app.post('/signup1', controller.webentry);
 // });
 
 app.post('/incomingsms', function(req, res) {
-    var twilio = require('twilio');
-    var twiml = new twilio.TwimlResponse();
-    twiml.message(function() {
-      this.body('The Robots are coming! Head for the hills!');
-    });
-    res.writeHead(200, {'Content-Type': 'text/xml'});
-    res.end(twiml.toString());
+  var twilio = require('twilio');
+  var twiml = new twilio.TwimlResponse();
+  twiml.message('The Robots are coming! Head for the hills!');
+  res.writeHead(200, {'Content-Type': 'text/xml'});
+  res.end(twiml.toString());
 });
 
 
