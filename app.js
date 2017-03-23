1444674c6d75db0a10b5e936e1428d00d5ffedb1
Dana Ress <danaress@MacBook-Air.local>
2017-03-23 17:34:48 -0600

    twilio

--- app.js
+++ app.js
@@ -41,18 +41,18 @@ const client = require('twilio')(accountSid, authToken);
 app.post('/signup1', controller.webentry);
 
 
-app.post("/incomingsms", function (request, response) {
-  console.log(req.body); 
-  response.send("<Response></Response>")
-});
-
-// app.post('/incomingsms', function(req, res) {
-//   var twilio = require('twilio');
-//   var twiml = new twilio.TwimlResponse();
-//   twiml.message('The Robots are coming! Head for the hills!');
-//   res.writeHead(200, {'Content-Type': 'text/xml'});
-//   res.end(twiml.toString());
+// app.post("/incomingsms", function (request, response) {
+//   console.log(req.body); 
+//   response.send("<Response></Response>")
 // });
+
+app.post('/incomingsms', function(req, res) {
+  var twilio = require('twilio');
+  var twiml = new twilio.TwimlResponse();
+  twiml.message('The Robots are coming! Head for the hills!');
+  res.writeHead(200, {'Content-Type': 'text/xml'});
+  res.end(twiml.toString());
+});
 
 // app.post('/incomingsms', controller.incomingsms);
 
